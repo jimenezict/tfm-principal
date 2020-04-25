6bbc9e3da5debe590450d8124086ce7774143aca
LENOVO-PC\Daniel <jimenezict@gmail.com>
2020-04-25 19:23:58 +0200

    Addition of the ContextLoadTests

---
+++ src/test/java/com/uoctfm/principal/ContextLoadTests.java
@@ -0,0 +1,13 @@
+package com.uoctfm.principal;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ContextLoadTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
