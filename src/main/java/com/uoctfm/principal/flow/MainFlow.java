be27f53feac9e4b66cd951e09984f370ab8dbc1b
LENOVO-PC\Daniel <jimenezict@gmail.com>
2020-07-05 17:51:50 +0200

    Reducing the loop frequency
    In order to study the memory performance, we expand the recurrence from 1 minute to 3

--- src/main/java/com/uoctfm/principal/flow/MainFlow.java
+++ src/main/java/com/uoctfm/principal/flow/MainFlow.java
@@ -23,7 +23,7 @@ public class MainFlow {
 
     private final Logger logger = getLogger(MainFlow.class);
 
-    @Scheduled(cron = "0 0/1 * 1/1 * ?")
+    @Scheduled(cron = "0 0/3 * * * ?")
     public void execute() {
         List<SystemConfigurationDTO> systemConfigurationList = systemConfiguration.getSystemConfiguration();
 
