137921570bbfdd762166ef93a175cb33e4eab8a3
LENOVO-PC\Daniel <jimenezict@gmail.com>
2020-08-09 22:49:06 +0200

    Adding a property for capturing the systemstatistics
    By now, the statistics will be captured if the boolean on the application properties values true.

--- src/main/java/com/uoctfm/principal/service/configuration/SystemStatisticsImpl.java
+++ src/main/java/com/uoctfm/principal/service/configuration/SystemStatisticsImpl.java
@@ -4,6 +4,7 @@ import com.uoctfm.principal.domain.configuration.SystemStatisticsDTO;
 import com.uoctfm.principal.repository.configuration.SystemStatisticsRepository;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -11,6 +12,8 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Service
 public class SystemStatisticsImpl implements SystemStatistics {
 
+    @Value("${service.main.systemstatistics:false}")
+    private Boolean systemStatistics;
     @Autowired
     private SystemStatisticsRepository systemStatisticsRepository;
 
@@ -18,10 +21,12 @@ public class SystemStatisticsImpl implements SystemStatistics {
 
     @Override
     public void insert(SystemStatisticsDTO systemStatisticsDTO) {
-        try{
-            systemStatisticsRepository.save(systemStatisticsDTO);
-        }catch (Exception e){
-            logger.error("Process {} could not save execution statistics: ", systemStatisticsDTO.getId(), e.getMessage());
+        if(systemStatistics) {
+            try {
+                systemStatisticsRepository.save(systemStatisticsDTO);
+            } catch (Exception e) {
+                logger.error("Process {} could not save execution statistics: ", systemStatisticsDTO.getId(), e.getMessage());
+            }
         }
     }
 }
