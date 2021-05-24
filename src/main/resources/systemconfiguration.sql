a1d85c65a2e11f120f5f32e69fbbe2948b1a60c4
jimenezict <jimenezict@gmail.com>
2021-05-24 21:29:53 +0200

    Adding new jcdecaux cities scripts

--- src/main/resources/systemconfiguration.sql
+++ src/main/resources/systemconfiguration.sql
@@ -1,23 +1,82 @@
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('sevilla','http://localhost:8085/status/es/seville/sevici','http://localhost:8085/location/es/sevici',true,true,true,true);
+values ('sevilla','http://localhost:8085/status/dublin','http://localhost:8085/location/dublin',true,true,true,true);
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('valencia','http://localhost:8085/status/es/valence/valenbisi','http://localhost:8085/location/es/valenbisi',true,true,true,true);
+values ('rouen','http://localhost:8085/status/rouen','http://localhost:8085/location/rouen',true,true,true,true);
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('santander','http://localhost:8085/status/es/santander/tusbic','http://localhost:8085/location/es/tusbic',true,true,true,true);
+values ('valence','http://localhost:8085/status/valence','http://localhost:8085/location/valence',true,true,true,true);
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('dublin','http://localhost:8085/status/ie/dublin/dublinbikes','http://localhost:8085/location/ie/dublinbikes',true,true,true,true);
+values ('toulouse','http://localhost:8085/status/toulouse','http://localhost:8085/location/toulouse',true,true,true,true);
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('marseille','http://localhost:8085/status/fr/marseille/levelo-mpm','http://localhost:8085/location/fr/levelo-mpm',true,true,true,true);
+values ('santander','http://localhost:8085/status/santander','http://localhost:8085/location/santander',true,true,true,true);
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('toulouse','http://localhost:8085/status/fr/toulouse/velo.toulouse','http://localhost:8085/location/fr/velo.toulouse',true,true,true,true);
+values ('luxembourg','http://localhost:8085/status/luxembourg','http://localhost:8085/location/luxembourg',true,true,true,true);
+
+-----
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('ljubljana','http://localhost:8085/status/si/ljubljana/bicikelj','http://localhost:8085/location/si/bicikelj',true,true,true,true);
+values ('lund','http://localhost:8085/status/lund','http://localhost:8085/location/lund',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('bruxelles','http://localhost:8085/status/bruxelles','http://localhost:8085/location/bruxelles',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('amiens','http://localhost:8085/status/amiens','http://localhost:8085/location/amiens',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('lillestrom','http://localhost:8085/status/lillestrom','http://localhost:8085/location/lillestrom',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('mulhouse','http://localhost:8085/status/mulhouse','http://localhost:8085/location/mulhouse',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('lyon','http://localhost:8085/status/lyon','http://localhost:8085/location/lyon',true,true,true,true);
+
+-----
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('ljubljana','http://localhost:8085/status/ljubljana','http://localhost:8085/location/ljubljana',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('seville','http://localhost:8085/status/seville','http://localhost:8085/location/seville',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('namur','http://localhost:8085/status/namur','http://localhost:8085/location/namur',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('nancy','http://localhost:8085/status/nancy','http://localhost:8085/location/nancy',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('creteil','http://localhost:8085/status/creteil','http://localhost:8085/location/creteil',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('cergy-pontoise','http://localhost:8085/status/cergy-pontoise','http://localhost:8085/location/cergy-pontoise',true,true,true,true);
+
+-----
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('vilnius','http://localhost:8085/status/vilnius','http://localhost:8085/location/vilnius',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('toyama','http://localhost:8085/status/toyama','http://localhost:8085/location/toyama',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('marseille','http://localhost:8085/status/marseille','http://localhost:8085/location/marseille',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('nantes','http://localhost:8085/status/nantes','http://localhost:8085/location/nantes',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('brisbane','http://localhost:8085/status/brisbane','http://localhost:8085/location/brisbane',true,true,true,true);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('besancon','http://localhost:8085/status/besancon','http://localhost:8085/location/besancon',true,true,true,true);
+
+-----
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
 values ('bicing','http://localhost:8082/status','http://localhost:8082/location',true,true,true,true);
@@ -26,16 +85,18 @@ insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocation
 values ('zaragoza','http://localhost:8083/status', 'http://localhost:8083/location',true,true,true,true);
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('mock10','http://localhost:9092/status/10', 'http://localhost:9092/location/10',true,true,true,false);
-
-insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
-values ('mock200','http://localhost:9092/status/200', 'http://localhost:9092/location/200',true,true,true,false);
-
-insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
 values ('cdmx','http://localhost:8089/status', 'http://localhost:8089/location',true,true,true,true);
 
 insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
 values ('manhattan','http://localhost:8090/status', 'http://localhost:8090/location',true,true,true,true);
+
+----
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('mock10','http://localhost:9092/status/10', 'http://localhost:9092/location/10',true,true,true,false);
+
+insert into uoc.systemconfiguration (name, systemstationendpoint, systemlocationendpoint, saveinfilesystem, saveintimeseries, saveingis, masterenable)
+values ('mock200','http://localhost:9092/status/200', 'http://localhost:9092/location/200',true,true,true,false);
 
 --  update uoc.systemconfiguration set masterenable = false;
 --
